f319699f468d11b6e1718ff55969ffa432053ed7
prattbj <104381242+prattbj@users.noreply.github.com>
2022-06-06 10:53:56 -0600

    Created Namespaces

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Game.Directing;
+using Game.Services;
+using Game.Casting;
+namespace Greed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
