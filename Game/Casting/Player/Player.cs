ea049b48372a97d5f8125a268b114d020e65d043
edw17003 <edw17003@byui.edu>
2022-06-07 20:24:41 -0600

    commit on 'main'

--- Game/Casting/Player/Player.cs
+++ Game/Casting/Player/Player.cs
@@ -2,6 +2,21 @@ namespace Game.Casting
 {
     class Player : Actor
     {
-        
+        private int direction = 0;
+
+        public Player() 
+        {
+            setText("#");
+        }
+
+        public void SetPosition() 
+        {
+            setX(this.direction * 5);
+        }
+
+        public void SetDirection(int direction)
+        {
+            this.direction = direction;
+        }
     }
 }
