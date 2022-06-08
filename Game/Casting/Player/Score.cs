db0f871ea835b3facf49c6e59a7f94be8cba77b8
JoshJ-WDD <joh21036@byui.edu>
2022-06-07 19:54:28 -0600

    initial build for score

--- Game/Casting/Player/Score.cs
+++ Game/Casting/Player/Score.cs
@@ -1,7 +1,28 @@
 namespace Game.Casting
 {
-    class Score : Actor
-    {
+    class Score : Actor{
+        private int score;
+
+        public Score() {
+        }
+
         
+        public getScore() {
+            return score;
+        }
+
+        public addScore(int score) {
+            this.score += score;
+            updateScoreText();
+        }
+
+        public minusScore(int score) {
+            this.score -= score;
+            updateScoreText();
+        }
+
+        private updateScoreText() {
+            this.text = $"Score: {this.score}";
+        }
     }
 }
