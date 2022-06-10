7c51b6ab72d8ed8e7911e9b3c841475e62d94aab
prattbj <104381242+prattbj@users.noreply.github.com>
2022-06-09 20:49:29 -0600

    Comments added

--- Game/Casting/Objects/Gem.cs
+++ Game/Casting/Objects/Gem.cs
@@ -1,8 +1,13 @@
 namespace Game.Casting
 {
+    //Gem holds the information about gems.
+    //It sets the values that pertain to gems
+    //whenever a gem is created
     class Gem : FallingObject
     {
         Random random = new Random();
+        //set the text to *, set score increment to the velocity,
+        //set it to a random color
         public Gem()
         {
             setText("*");
